84656d5c162075bc4f3c90ca2b08d45cb9d291ee
Tim Murphy <tim@26tp.com>
2013-07-09 23:13:43 +1000

    New CachedRepositoryTests.

---
+++ Projects/RepositoryMagic.Tests/CachedRepositoryTests.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RepositoryMagic.Tests
+{
+    [TestClass]
+    public abstract class CachedRepositoryTests : IRepositoryTests
+    {
+        // todo: specifically test the caching.
+    }
+}
